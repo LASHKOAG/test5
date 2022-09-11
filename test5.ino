a0a65bcf6c3eff82f1315a71c5280c0e1b0482a1
oztm <lashkoag@yandex.ru>
2022-09-11 09:05:36 +0600

    first commit

---
+++ test5.ino
@@ -0,0 +1,10 @@
+
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second                     // wait for a second
+}
